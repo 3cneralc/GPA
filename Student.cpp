23f68d7c28005b45b2ae61687fba7f92931cfbe4
Clarence Leong <clarenceleong@outlook.com>
2016-01-23 06:17:54 +0800

    Updated new Student.cpp

---
+++ Student.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include "Student.h"
+using namespace std;
+
+void Student::setStudentName(string name) {
+    studentName = name;
+}
+
+string Student::getStudentName() {
+    return studentName;
+}
+
+void Student::setAttemptedModule(string moduleAttempted) {
+    attemptedModule = moduleAttempted;
+}
+
+string Student::getAttemptedModule(){
+    return attemptedModule;
+}
+
+void Student::setGrade(char g){
+    grade = g;
+}
+
+char Student::getGrade() {
+    return grade;
+}
+
+void Student::setScore(int s) {
+    score = s;
+}
+
+int Student::getScore() {
+    return score;
+}
+
+void Student::setCreditUnits(int cu) {
+    creditUnits = cu;
+}
+
+int Student::getCreditUnits() {
+    return creditUnits;
+}
